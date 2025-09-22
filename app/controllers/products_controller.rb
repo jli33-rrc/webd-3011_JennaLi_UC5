224598bbf344c1f3e7028a247e49e1a1fadbf08e
Jenna <jli33@academic.rrc.ca>
2025-09-22 10:47:21 -0500

    Updated the Products controller and displayed all products. [Part 1, Step 16]

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    # Part 1, Step 13
+    @products = Product.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Part 1, Step 15
+  get '/products', to: 'products#index'
 end
