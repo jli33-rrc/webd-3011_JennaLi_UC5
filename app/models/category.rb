068059db4515daf9c7accffe6b3142354e9adbd9
Jenna <jli33@academic.rrc.ca>
2025-09-22 12:36:34 -0500

    Added associations to the Product and Category models. [Part 2, Step 4]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+    # Part 2, Step 4
+    has_many :products, dependent: :destroy
+    validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
+    # Part 1, Step 6
     validates :title, presence: true
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    # Part 2, Step 4
+    belongs_to :category
 end
